63e3ba202fee1549c8420f870ad9a79da4d6459a
Hugo <58655174+Rhodless@users.noreply.github.com>
2022-06-04 15:39:25 +0200

    [+] Added BlockPlacePower power type

---
+++ src/main/java/fr/kohei/mugiwara/power/BlockPlacePower.java
@@ -0,0 +1,29 @@
+package fr.kohei.mugiwara.power;
+
+import fr.kohei.mugiwara.roles.RolesType;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public abstract class BlockPlacePower extends ClickPower {
+
+    public static BlockPlacePower findPower(RolesType.MURole role, ItemStack item) {
+        BlockPlacePower power = null;
+
+        for (Power rolePower : role.getPowers()) {
+            if (!(rolePower instanceof BlockPlacePower)) continue;
+
+            BlockPlacePower damagePower = (BlockPlacePower) rolePower;
+
+            ItemStack powerItem = damagePower.getItem();
+            if (powerItem.getType() == item.getType() && powerItem.hasItemMeta()
+                    && powerItem.getItemMeta().getDisplayName().equals(item.getItemMeta().getDisplayName())) {
+                power = damagePower;
+            }
+        }
+
+        return power;
+    }
+
+    public abstract boolean onEnable(Player player);
+
+}
