305e7447e823608f483e787d65d0ddc284c68b21
Hugo <58655174+Rhodless@users.noreply.github.com>
2022-06-09 13:34:27 +0200

    Update Arrow.java

--- src/main/java/fr/kohei/mugiwara/utils/Arrow.java
+++ src/main/java/fr/kohei/mugiwara/utils/Arrow.java
@@ -10,9 +10,7 @@ import java.util.HashMap;
 import java.util.UUID;
 
 public class Arrow {
-    public static final HashMap<UUID, String> ARROWS = new HashMap<>();
-
-    public static void setArrow(Player player, Player target, int seconds) {
+    public static void setArrow(Player player, Player target, int seconds, boolean hideName) {
         final UUID playerUUID = player.getUniqueId();
         final UUID targetUUID = target.getUniqueId();
 
@@ -22,22 +20,28 @@ public class Arrow {
             @Override
             public void run() {
                 timer--;
+                Player player = Bukkit.getPlayer(playerUUID);
+                Player target = Bukkit.getPlayer(targetUUID);
                 if (timer <= 0) {
+                    Mugiwara.getInstance().removeActionBar(player, "arrow");
                     cancel();
                     return;
                 }
 
-                Player player = Bukkit.getPlayer(playerUUID);
-                Player target = Bukkit.getPlayer(targetUUID);
 
                 if (player == null || target == null) return;
 
                 double distance = player.getLocation().distance(target.getLocation());
                 String arrow = LocationUtils.getArrow(player.getLocation(), target.getLocation());
 
-                String display = "&6" + target.getName() + " " + arrow + " " + distance;
-                ARROWS.put(playerUUID, display);
+                String targetName = hideName ? "&k" + target.getName() + "&r" : target.getName();
+                String display = "&6" + targetName +" " + arrow + " " + distance;
+                Mugiwara.getInstance().addActionBar(player, display, "arrow");
             }
         }.runTaskTimer(Mugiwara.getInstance(), 0, 20);
+    }
+
+    public static void setArrow(Player player, Player target, int seconds) {
+        setArrow(player, target, seconds, false);
     }
 }
